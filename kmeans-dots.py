ba2a26114c3b45e7255d1bd0ba279e5bc6b7bd7a
ermvad <ermvad@yandex.ru>
2019-04-14 23:20:54 +0300

    2

--- kmeans-dots.py
+++ kmeans-dots.py
@@ -5,7 +5,14 @@ import matplotlib.pyplot as plt
 
 filename = "resource/dots.csv"
 clusters = 3
-epsilon = 2
+epsilon = 1
+
+
+def make_plot(d1, d2, i):
+    plt.scatter(d1[:, 0], d1[:, 1], color='g')
+    plt.scatter(d2[:, 0], d2[:, 1], color='r')
+    plt.savefig('imagess/' + str(i) + '.png', transparent=False, frameon=False, quality=100)
+    plt.clf()
 
 
 def euclidian(a, b):
@@ -13,7 +20,9 @@ def euclidian(a, b):
 
 
 def kmeans(data, cl, eps=1):
+    iteration = 0
     centers = numpy.random.permutation(numpy.unique(data, axis=0))[:cl]
+    make_plot(data, centers, iteration)
     while True:
         labels = numpy.zeros(data.shape[0])
         for d in range(data.shape[0]):
@@ -22,9 +31,13 @@ def kmeans(data, cl, eps=1):
                 e = numpy.append(e, [[euclidian(data[d,:], centers[c,:]), c]], axis=0)
             labels[d] = e[numpy.argmin(e[:, 0])][1]
         new_centers = numpy.array([data[labels == i, :].mean(0) for i in range(cl)])
+        iteration += 1
         if numpy.mean(new_centers - centers) < eps:
+            make_plot(data, new_centers, iteration)
             break
         centers = new_centers
+        make_plot(data, centers, iteration)
+    print('Total iterations: ' + str(iteration))
     return new_centers
 
 
@@ -36,9 +49,6 @@ def main():
             cords = numpy.append(cords, [[int(row[0]), int(row[1])]], axis=0)
     
     kmeans_centers = kmeans(cords, clusters, epsilon)
-    plt.scatter(cords[:, 0], cords[:, 1], color='g')
-    plt.scatter(kmeans_centers[:, 0], kmeans_centers[:, 1], color='r')
-    plt.show()
 
 
 if __name__ == "__main__":
